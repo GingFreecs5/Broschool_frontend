1c33476a5e97ac45fedc96d80a320dbe6d2e8f75
Yassine Akkab <yassin.akkab@eurafric-information.com>
2023-05-02 18:06:01 +0100

    update

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,40 +1,63 @@
 import React from "react";
 import "../App.css";
 import MyButton from "../components/login/MyButton";
+import ImgLearning from "../assets/elearning-portals-cover-picture 1.png";
 function Login() {
   return (
     <div className="bgImg">
-      <div className="d-flex container loginContainer">
-        <div className="card_class row card p-5  mx-auto ">
-          <div className="container">
+      <div className="d-flex loginContainer">
+        <div className="card_class mt-5 gap-5  card p-5   mx-auto ">
+          <div className=" row align-items-stretch  ">
             <span
-              className="row"
+              className="col"
               style={{
                 color: "#4680D6",
-                fontWeight: 400,
+                fontWeight: 800,
+                fontSize: 30,
                 lineHeight: "24px",
               }}
             >
               Broschool
             </span>
             <MyButton
-              className={"row my-3"}
-              bgColor={"#4D77E3"}
-              color={"white"}
-              text={"Login"}
-              paddingX={"30px"}
-              paddingY={"3px"}
-              textSize={17}
-            />
-            <MyButton
-              className={"row"}
+              className={"col"}
               bgColor={"#4D77E3"}
               color={"white"}
               text={"Register"}
-              paddingX={"20px"}
-              paddingY={"3px"}
               textSize={17}
             />
+          </div>
+          <div className="d-flex row">
+            <div className="row flex-column gap-3 justify-content-center align-items-center ">
+              <span
+                className="col"
+                style={{
+                  color: "#000000",
+
+                  fontSize: 20,
+                  lineHeight: "24px",
+                }}
+              >
+                Login to your account
+              </span>
+              <input
+                type="email"
+                className=" col form-control rounded"
+                placeholder="Enter email"
+              ></input>
+              <input
+                type="email"
+                className="col form-control rounded"
+                placeholder="Enter email"
+              ></input>
+            </div>
+            <div className="row ">
+              <img
+                src={ImgLearning}
+                alt="ImgLearning"
+                style={{ height: 180, width: 180 }}
+              />
+            </div>
           </div>
         </div>
       </div>
